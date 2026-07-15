b9f79b2495fa18d33d90087bd69361978e702469
superfluid-helium <144962990+superfluid-helium@users.noreply.github.com>
2026-07-15 12:58:04 -0500

    periodogram for sunspots

---
+++ Code/sunspots_periodogram.R
@@ -0,0 +1,17 @@
+library(TSA)
+library(scorepeak)
+
+my.data.sunspots <- sunspot_data |>
+  dplyr::filter(year>=1702)
+sp.sunspots <- TSA::periodogram(my.data.sunspots$ma3, plot=F)
+
+plot(1/sp.sunspots$freq,sp.sunspots$spec,type='l',
+     xlab="Periods",
+     ylab="Spec",
+     main=paste0('Sunspots periodogram (data after 1702 inclusive)'),
+     xlim=c(0,150))
+local_peaks <- scorepeak::detect_localmaxima(sp.sunspots$spec)
+df.sunspots.periodogram <-  data.frame(sp.sunspots$spec[local_peaks], 1/sp.sunspots$freq[local_peaks])
+
+write.csv(df.sunspots.periodogram,
+          file = "/Users/sofia/programming/hussrp-2026/sunspots_periodogram_local_peaks.csv")
--- hussrp-2025/sunspots_eruptions_moving_average.R
+++ hussrp-2025/sunspots_eruptions_moving_average.R
@@ -10,7 +10,7 @@ sunspot_data <- sunspot_data |>
 ggplot(eruptions_filled, aes(x=YEAR)) +
   geom_line(aes(y=count), alpha=0.5) +
   geom_line(aes(y=ma3), size=1.2) +
-  labs(title="Annual Significant Volcanic Eruptions (1600-2025) with 10-Year Moving Average",
+  labs(title="Annual Significant Volcanic Eruptions (1600-2025) with 3-Year Moving Average",
      x="Year", y="Eruptions",
      caption="Gray=annual count, Blue=10-year moving average") +
   theme_minimal() +
@@ -19,7 +19,7 @@ ggplot(eruptions_filled, aes(x=YEAR)) +
 ggplot(sunspot_data, aes(x=year)) +
   geom_line(aes(y=sunspots), alpha=0.5) +
   geom_line(aes(y=ma3), size=1.2) +
-  labs(title="Yearly Mean Sunspot Number (1700-2025) with 10-Year Moving Average",
+  labs(title="Yearly Mean Sunspot Number (1700-2025) with 3-Year Moving Average",
      x="Year", y="Sunspots",
      caption="Gray=annual count, Blue=10-year moving average") +
   theme_minimal() +
